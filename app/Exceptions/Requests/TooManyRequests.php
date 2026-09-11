9b48fd99ad394d3735f1d606ac05f9600ca9cd3e
Gustavo Souza Santos <gustavo.santos@hospitalcirurgia.com.br>
2026-09-11 14:08:02 -0300

    feat: add throttle to router send-code

--- app/Exceptions/Requests/TooManyRequests.php
+++ app/Exceptions/Requests/TooManyRequests.php
@@ -6,11 +6,12 @@ use App\Exceptions\DomainException;
 
 class TooManyRequests extends DomainException
 {
-    public function __construct(?string $message = null)
+    public function __construct(?string $message = null, ?array $extra = [])
     {
         parent::__construct(
             message: $message ?? 'Muitas tentativas. Aguarde um momento e tente novamente.',
             status: 429,
+            extra: $extra
         );
     }
 }
